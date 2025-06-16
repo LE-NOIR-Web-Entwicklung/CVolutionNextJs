a2633b0610fc27a5b83c7a64637f8576da3090d6
Jan Eggenberger <j.eggenberger@federerag.ch>
2025-06-16 07:01:18 +0200

    changes to key

--- cvolution/lib/resend.ts
+++ cvolution/lib/resend.ts
@@ -2,7 +2,7 @@
 
 import { Resend } from "resend"; 
 
-const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async (name: string, email: string, service: string) => {
     await resend.emails.send({
