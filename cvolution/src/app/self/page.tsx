28aba2ce585894ae8196a629e4903537b880df29
Jan Eggenberger <94107361+FEDIT-JE@users.noreply.github.com>
2025-07-31 13:35:08 +0200

    Update page.tsx

--- cvolution/src/app/self/page.tsx
+++ cvolution/src/app/self/page.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from "react";
 
 export default function Self() {
-  const targetDate = new Date("2025-08-01T00:00:00").getTime();
+  const targetDate = new Date("2025-08-31T00:00:00").getTime();
   const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
 
   function calculateTimeLeft() {
