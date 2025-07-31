b27756d695eea611bf6dc1ba2f9a4f9652902251
Jan Eggenberger <94107361+FEDIT-JE@users.noreply.github.com>
2025-07-31 13:36:51 +0200

    Update page.tsx

--- cvolution/src/app/self/page.tsx
+++ cvolution/src/app/self/page.tsx
@@ -39,7 +39,7 @@ export default function Self() {
     <div className="min-h-screen bg-[#204878] text-white flex flex-col items-center justify-center">
       <h1 className="text-4xl font-bold mb-6">Self-Service <span className="text-blue-300">Coming Soon!</span></h1>
       <p className="text-lg mb-8">
-        Das Self-Service Angebot ist ab <strong>1. August 2025</strong> verfügbar.
+        Das Self-Service Angebot ist ab <strong>31. August 2025</strong> verfügbar.
       </p>
       <div className="flex space-x-4 text-center">
         <div className="bg-[#1a3a66] p-4 rounded-lg shadow-lg">
