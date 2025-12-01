6f6dda6dfc0e0147a0c9a1277823800f27e7b31b
Jan Eggenberger <94107361+FEDIT-JE@users.noreply.github.com>
2025-12-01 05:36:07 +0100

    Revert "Cvexport"

--- cvolution/next.config.ts
+++ cvolution/next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  outputFileTracingRoot: __dirname,
-  reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   /* config options here */
 };
 
