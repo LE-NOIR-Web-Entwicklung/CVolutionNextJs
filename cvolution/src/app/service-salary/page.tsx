5571af85fef61df0242c4fd14f58a9ce8b500151
Jan Eggenberger <j.eggenberger@federerag.ch>
2026-01-09 14:21:57 +0100

    arbeitstag

--- cvolution/src/app/service-salary/page.tsx
+++ cvolution/src/app/service-salary/page.tsx
@@ -213,7 +213,7 @@ export default function ServiceSalary() {
                 </h4>
                 <ul className="list-disc list-inside text-base leading-relaxed mb-6 space-y-1">
                   <li>Erstellung einer individuellen Lohnanalyse auf Basis Ihrer Angaben</li>
-                  <li>Zustellung eines vollständigen PDF-Dokuments innerhalb von 24 Stunden</li>
+                  <li>Zustellung eines vollständigen PDF-Dokuments innerhalb von 1 Arbeitstag</li>
                   <li>Argumentationsgrundlage für Ihre weitere Planung</li>
                   <li>Transparenter Vergleich mit branchenüblichen Gehältern</li>
                   <li>Einschätzung basierend auf Position, Erfahrung und Markt</li>
