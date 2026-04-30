1dc05f2b1cba83d4d35b30d8fdf96ad9c3f6e9cb
Jan Eggenberger <j.eggenberger@federerag.ch>
2026-04-30 08:06:15 +0200

    Update page.tsx

--- cvolution/src/app/admin/page.tsx
+++ cvolution/src/app/admin/page.tsx
@@ -584,7 +584,7 @@ const AdminContent: React.FC = () => {
       </header>
 
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-        <Tabs defaultValue="users" className="space-y-6">
+        <Tabs defaultValue="coupons" className="space-y-6">
           <TabsList className="bg-white border border-gray-200 h-auto p-1">
             <TabsTrigger value="coupons" className="gap-2 data-[state=active]:bg-[#204878] text-black data-[state=active]:text-white">
               <TicketPercent className="h-4 w-4" />
